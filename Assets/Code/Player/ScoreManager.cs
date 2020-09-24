eb520ad96f2d69c1e1553fedea3f2da5580ef9c2
Lee Martin <l.m_li@outlook.com>
2020-09-24 16:03:53 +0100

    Added the base score mechanism, it gives score over time increased by the current acceleration

---
+++ Assets/Code/Player/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager singleton;
+
+    public float Score = 0;
+    public float BaseScore = 1;
+    public float MultiplierForSpeed = 10;
+
+    private void Awake()
+    {
+        if(singleton == null)
+        {
+            singleton = this;
+        }
+    }
+
+    private void Update()
+    {
+        if(PlayerController.Singleton.acceleration > 0)
+        {
+            Score += BaseScore * (PlayerController.Singleton.acceleration * MultiplierForSpeed) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Code/Player/UI/ScoreRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreRenderer : MonoBehaviour
+{
+    public Text score;
+
+    private void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        score.text = "Score - " + Mathf.FloorToInt(ScoreManager.singleton.Score).ToString();
+    }
+}
