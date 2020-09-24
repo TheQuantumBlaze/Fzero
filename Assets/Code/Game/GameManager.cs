0dafe79632d6c4e98d8076faca55accec1d1aab5
Lee Martin <l.m_li@outlook.com>
2020-09-24 23:34:14 +0100

    Added barrel spawning mech

--- Assets/Code/Game/GameManager.cs
+++ Assets/Code/Game/GameManager.cs
@@ -7,6 +7,10 @@ public class GameManager : MonoBehaviour
     public static GameManager singleton;
     public Vector3 spawnPoint = new Vector3();
 
+    public GameObject barrelPrefab;
+
+    public List<Vector3> barrelSpawn = new List<Vector3>();
+
     private void Awake()
     {
         if (singleton == null)
@@ -15,6 +19,15 @@ public class GameManager : MonoBehaviour
 
     public void spawnPlayer()
     {
+        SpawnBarrels();
         PlayerController.Singleton.gameObject.transform.position = spawnPoint;
+    }
+
+    public void SpawnBarrels()
+    {
+        foreach(Vector3 vec in barrelSpawn)
+        {
+            GameObject go = Instantiate(barrelPrefab, vec, Quaternion.identity);
+        }
     }
 }
