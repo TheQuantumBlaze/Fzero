835d96ff7632b736ddde3868167a36f78515c7ac
Lee Martin <l.m_li@outlook.com>
2020-09-24 20:15:59 +0100

    Added the abaility to creat a spawn point for the player defined as a red dot on the image

---
+++ Assets/Code/Game/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager singleton;
+    public Vector3 spawnPoint = new Vector3();
+
+    private void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+    }
+
+    public void spawnPlayer()
+    {
+        PlayerController.Singleton.gameObject.transform.position = spawnPoint;
+    }
+}
